736748079e708259d9eaba573b59bdb07179f55f
Shruti082003 <shrutirathod2003@gmail.com>
2024-05-23 13:07:12 +0530

    weather app

--- src/components/SearchEngine.js
+++ src/components/SearchEngine.js
@@ -12,7 +12,7 @@ function SearchEngine({ query, setQuery, search }) {
         onChange={(e) => setQuery(e.target.value)}
         onKeyPress={search}
       />
-      <button><i className="fas fa-search" style={{ fontSize: "18px" }}></i></button>
+      
     </div>
   );
 }
